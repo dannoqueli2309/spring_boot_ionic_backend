a36ad5ba08019db90948fa18928bf5a3e2a5b99f
Danilo Noqueli <dannoqueli@gmail.com>
2018-08-26 18:17:27 -0300

    many to many produto e categoria

--- src/main/java/com/aplicacao/cursomc/CursomcApplication.java
+++ src/main/java/com/aplicacao/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.aplicacao.cursomc;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.aplicacao.cursomc.domain.Categoria;
+import com.aplicacao.cursomc.domain.Produto;
 import com.aplicacao.cursomc.repositories.CategoriaRepository;
+import com.aplicacao.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -24,6 +31,18 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto prod1 = new Produto(null,"Computador",2000.00);
+		Produto prod2 = new Produto(null,"Impressora",800.00);
+		Produto prod3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(prod1,prod2,prod3));// Arrays. asList passa varios elementos 
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+		
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3));
 	}
 }
