ddd8af704fe595fb0ad7fbf56f2b84646417500a
Danilo Noqueli de Oliveira <Danilo.Noqueli@capco.com>
2020-06-30 16:07:29 -0300

    Autorização spring  security

--- src/main/java/com/aplicacao/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/aplicacao/cursomc/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(categoriaService);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO) {
 		Categoria categoria = service.fromDTO(categoriaDTO);
@@ -42,7 +44,8 @@ public class CategoriaResource {
 				.toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody CategoriaDTO categoriaDTO, @PathVariable Integer id){
 		Categoria categoria = service.fromDTO(categoriaDTO);
@@ -50,7 +53,8 @@ public class CategoriaResource {
 		categoria = service.update(categoria);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE) // protocolo HTTP tipo GET passando id
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
